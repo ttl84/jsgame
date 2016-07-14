50351721f58154c1ce3028da28ec88cff30d97d3
ultramailman <cyrus9212@gmail.com>
2016-07-14 00:13:04 -0700

    Camera now centers on world.cameraFocus

--- main.js
+++ main.js
@@ -168,9 +168,15 @@ World.prototype.addObject = function(o) {
   this.objects.push(o);
 }
 World.prototype.drawObjects = function(ctx) {
+  ctx.save();
+  var focusObj = this.objects[this.cameraFocus];
+  ctx.translate(-focusObj.x + ctx.canvas.width / 2, -focusObj.y + ctx.canvas.height / 2);
+
   for(var i = 0; i < this.objects.length; i++) {
     this.objects[i].draw(ctx);
   }
+
+  ctx.restore();
 }
 World.prototype.updateObjects = function(input, dt) {
   for(var i = 0; i < this.objects.length; i++) {
